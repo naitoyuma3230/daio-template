a25546c57aff91fd23af063e816698b51208711c
NantoYuma <naitoyuma@outlook.jp>
2022-03-23 19:38:09 +0900

    all

--- js/loading.js
+++ js/loading.js
@@ -1,8 +1,8 @@
 // //logoの表示
-// $(window).on("load", function () {
-// 	$("#splash").delay(1500).fadeOut("slow"); //ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-// 	$("#splash_logo").delay(1200).fadeOut("slow"); //ロゴを1.2秒（1200ms）待機してからフェードアウト
-// });
+$(window).on("load", function () {
+	$("#splash").delay(1500).fadeOut("slow"); //ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+	$("#splash_logo").delay(1200).fadeOut("slow"); //ロゴを1.2秒（1200ms）待機してからフェードアウト
+});
 
 $(window).on("load", function () {
 	$("#splash-logo").delay(2000).fadeOut("slow"); //ロゴを1.2秒でフェードアウトする記述
